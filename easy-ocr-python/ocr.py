3164ef9351eb887d8bd5dd757a4d02a947e2291a
Martin Gaido <martin.gaido@gmail.com>
2021-04-10 11:11:47 -0300

    Update OCR

--- easy-ocr-python/ocr.py
+++ easy-ocr-python/ocr.py
@@ -3,10 +3,10 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-IMAGE_PATH = 'bsas2.jpg'
+IMAGE_PATH = 'demo.jpg'
 #IMAGE_PATH = 'surf.jpeg'
 
-reader = easyocr.Reader(['es'])
+reader = easyocr.Reader(['en'])
 result = reader.readtext(IMAGE_PATH)
 print(result)
 
@@ -24,14 +24,22 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 # multiple lines
 img = cv2.imread(IMAGE_PATH)
-spacer = 100
-for detection in result: 
-    top_left = tuple(detection[0][0])
-    bottom_right = tuple(detection[0][2])
-    text = detection[1]
-    img = cv2.rectangle(img,top_left,bottom_right,(0,255,0),3)
-    img = cv2.putText(img,text,(20,spacer), font, 0.5,(0,255,0),2,cv2.LINE_AA)
-    spacer+=15
-    
+spacer = 50
+for detection in result:
+	top_left = tuple(detection[0][0])
+	bottom_right = tuple(detection[0][2])
+	text = detection[1]
+	img = cv2.putText(img, text, (20, spacer), font, 1, (255,255,255), 2, cv2.LINE_AA)
+	spacer+=35
+
+# spacer = 100
+# for detection in result:
+#     top_left = tuple(detection[0][0])
+#     bottom_right = tuple(detection[0][2])
+#     text = detection[1]
+#     img = cv2.rectangle(img,top_left,bottom_right,(0,255,0),3)
+#     img = cv2.putText(img,text,(20,spacer), font, 0.5,(0,255,0),2,cv2.LINE_AA)
+#     spacer+=15
+
 plt.imshow(img)
 plt.show()
